0891a1aa24057401f0c7be2f58714db2584d1961
Ted Heikkila <ted.heikkila@gmail.com>
2021-07-27 10:34:42 -0500

    added avatar & updated model index

--- models/index.js
+++ models/index.js
@@ -1,7 +1,13 @@
 const User = require('./User');
 const Tracks = require('./Tracks');
+const Avatar = require('./Avatar');
 
 User.hasMany(Tracks, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasOne(Avatar, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
@@ -10,4 +16,8 @@ Tracks.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Tracks };
+Avatar.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Tracks, Avatar };
